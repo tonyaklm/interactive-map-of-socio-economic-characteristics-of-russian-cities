1b771c4c5ac568c11db57990900877256fa70f82
vikahse <vvkovalevskaya_1@edu.hse.ru>
2024-04-19 01:07:18 +0300

    register-login

--- map/app/routes.py
+++ map/app/routes.py
@@ -10,13 +10,97 @@ import requests
 import json
 import os
 from folium.features import GeoJson, GeoJsonTooltip, GeoJsonPopup
+<<<<<<< HEAD
 from app.map import color_region_by_id, data, add_circle_marker, add_marker, add_colorbar
+=======
+from app.map import color_region_by_id, data, add_circle_marker, add_marker
+from app.forms import LoginForm, RegisterForm
+from flask import Flask, render_template, flash, redirect, request, session, logging, url_for
+from app.models import User
+from app import db
+>>>>>>> c91245e (register-login)
 
 cur_map_name = "map"
 
 
+<<<<<<< HEAD
 @app.route("/")
 def render_map():
+=======
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if User.query.filter_by(login=form.login.data).first() is None:
+            new_user = User(username=form.username.data, login=form.login.data)
+            new_user.hash_password(form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+
+            # access_token = new_user.get_token()
+            # response = jsonify({'message': f'User-{new_user.username} registered successfully'})
+            # set_access_cookies(response, access_token)
+            # return response, 201
+            flash('You have successfully registered', 'success')
+            return redirect(url_for('login'))
+        else:
+            # return jsonify({'message': 'Existing user'}), 400
+            flash('Such login already exists', 'fail')
+            return redirect(url_for('login'))
+    else:
+        return render_template('register.html', form=form)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+
+    if request.method == 'POST' and form.validate:
+        user = User.query.filter_by(login=form.login.data).first()
+        if user is None:
+            # return jsonify({'message': 'Invalid username'}), 401
+            return render_template('login.html', form=form)
+        else:
+            if user is not None and user.verify_password(form.password.data):
+                # access_token = user.get_token()
+                flash('You have successfully logged in.', "success")
+                session['logged_in'] = True
+                # session['email'] = user.email
+                session['username'] = user.username
+
+                # set_access_cookies(response, access_token)
+                # return response, 200
+                return redirect(url_for('render_map'))
+            else:
+                # return jsonify({'message': 'Invalid password'}), 401
+                flash('Login or Password Incorrect', "Danger")
+
+                return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+#     
+# @app.after_request
+# def refresh_expiring_jwts(response):
+#     try:
+#         exp_timestamp = get_jwt()["exp"]
+#         now = datetime.now(timezone.utc)
+#         target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+#         if target_timestamp > exp_timestamp:
+#             access_token = create_access_token(identity=get_jwt_identity())
+#             set_access_cookies(response, access_token)
+#         return response
+#     except (RuntimeError, KeyError):
+#         return response
+#     
+
+@app.route("/map/")
+def render_map():
+    # print(os.listdir("../map/templates"))
+>>>>>>> c91245e (register-login)
     folium_map = folium.Map(location=[69, 88], zoom_start=3, control_scale=True)
     colormap = add_colorbar(folium_map, "Population")
     data.apply(add_circle_marker, axis=1, args=(folium_map, "Population", colormap,))
@@ -53,7 +137,7 @@ def map():
     return render_template(filename)
 
 
-@app.route('/indicator')
+@app.route('/indicator/')
 def chosen_indicator():
     global cur_map_name
 
